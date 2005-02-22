e9e00b10730676e7adb21808148bb2144f2151ac
innin <innin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-02-22 16:18:15 +0000

    Moved remotely
    git-svn-id: https://plugins.svn.wordpress.org/geshi-syntax-colorer/trunk@837 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ geshi_plugin.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: GeshiSyntaxColorer
+Version: 0.1
+Plugin URI: http://www.regex.ru
+Description: Program languages colorer based on <a href="http://qbnz.com/highlighter/index.php">GeSHI</a> engine
+Author: Mikhail Kyurshin
+Author URI: http://www.regex.ru
+*/
+
+include(ABSPATH.'/wp-content/plugins/geshi.php');
+
+function GeshiSyntaxColorer($text) {
+    $codes = array();
+    $str = uniqid('');
+    $i=0;
+    while (preg_match("#<code lang=['\"]([a-zA-Z0-9_-]+)['\"]>(.*?)</code>#s", $text, $matches)) {
+        $i++;
+        $codes[$i]['lang'] = $matches[1];
+        $codes[$i]['code'] = $matches[2];
+        $text = preg_replace("#<code lang=['\"][a-zA-Z0-9_-]+['\"]>.*?</code>#s", $str.$i, $text, 1);
+    }
+    
+    
+    $i=0;
+    while (preg_match("#".$str."([0-9]+)#", $text, $matches)) {
+        $i++;
+        $geshi = new GeSHi($codes[$i]['code'], $codes[$i]['lang'], ABSPATH.'/wp-content/plugins/geshi/');
+        $text = preg_replace("#".$str."([0-9]+)#", "<code>".$geshi->parse_code()."</code>", $text, 1);
+    }
+    return $text;
+}
+
+function GeshiSyntaxColorer_popup_help($name,$helpvar,$windowW,$windowH) {
+    $out = $name;
+    $out .= 'Use <code lang="lang">your code</code> tag to highlight code. Available langs can be found in GeShi documentation';
+    print $out;
+}
+
+
+// And now for the filters
+add_filter('the_content', 'GeshiSyntaxColorer', 6);
+add_filter('the_excerpt', 'GeshiSyntaxColorer', 6);
+add_filter('comment_text', 'GeshiSyntaxColorer', 6);
+
+?>
